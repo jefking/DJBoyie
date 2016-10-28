e9cf6569699e2786b7d038ea6c0ba168f532bd6c
Jef King <jefkin@microsoft.com>
2016-10-28 09:24:57 +0300

    nice

--- Data/Storage.cs
+++ Data/Storage.cs
@@ -3,18 +3,28 @@ public class Storage
         #region Members
         //Table Storage Connection
         private readonly string tableConnection = ConfigurationManager.AppSettings["StoryDialogueStore"];
+
+        private static bool msgTableCreated;
+        private static bool storyTableCreated;
         #endregion
 
         public async Task Save(Message msg)
         {
-            var msgTable = new TableStorage("message", tableConnection);
-            await msgTable.CreateIfNotExists();
-            await msgTable.Save(msg);
+            var table = new TableStorage("message", tableConnection);
+            if !(msgTableCreated)
+            {
+                msgTableCreated = await table.CreateIfNotExists();
+            }
+
+            await table.Save(msg);
         }
         public async Task Save(PersonalStory story)
         {
-
                 var table = new TableStorage("userprofile", tableConnection);
+            if !(storyTableCreated)
+            {
+                storyTableCreated = await table.CreateIfNotExists();
+            }
                 await table.CreateIfNotExists();
                 await table.InsertOrReplace(entity);
         }
